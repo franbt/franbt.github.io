45a183b04b5d9c77f2f4b1e1e551fe5eefc41724
franbt <franciscojosebermejotarifa@gmail.com>
2026-02-01 13:02:41 +0100

    Change sun light position in main.js

--- marco/main.js
+++ marco/main.js
@@ -17,7 +17,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
 
 const sun = new THREE.DirectionalLight(0xffffff, 3); 
-sun.position.set(-150, 200, 120); 
+sun.position.set(150, 200, -120); 
 sun.color.setRGB(1, 0.95, 0.9); 
 sun.castShadow = true;
 scene.add(sun);
